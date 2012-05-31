66e832ae72792d32eb4b777b683c1084f8e732ed
Arnaud Delattre <arnaud.delattre+imac@gmail.com>
2012-05-31 08:42:47 +0200

    init Initial project

---
+++ TableViewTest/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  TableViewTest
+//
+//  Created by Alain Joliveau on 31/05/12.
+//  Copyright (c) 2012 Alike-Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ TableViewTest/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  TableViewTest
+//
+//  Created by Alain Joliveau on 31/05/12.
+//  Copyright (c) 2012 Alike-Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
